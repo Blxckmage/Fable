521d7aff5402c224f914791c15a2e3b1f3163f41
Blxckmage <fazadoang15@gmail.com>
2024-03-05 12:07:43 +0700

    feat: add blog post component

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,14 +1,17 @@
 import Link from "next/link";
+import FormattedDate from "./FormattedDate";
 
-const BlogPost = ({ title, date, summary }) => {
+const BlogPost = ({ title, slug, id, date, summary }) => {
   return (
-    <Link href="/">
+    <Link href={`/post/${id}/${slug}`}>
       <article className="mb-6 md:mb-8">
         <header className="flex flex-col justify-between md:flex-row md:items-baseline">
           <h2 className="mb-2 cursor-pointer text-lg font-medium text-ctp-text md:text-xl">
             {title}
           </h2>
-          <time className="flex-shrink-0 text-ctp-text">{date}</time>
+          <time className="flex-shrink-0 text-ctp-text">
+            <FormattedDate date={date} />
+          </time>
         </header>
         <main>
           <p className="hidden leading-8 text-ctp-text md:block">{summary}</p>
